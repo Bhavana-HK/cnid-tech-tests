025a7b1f07b573500cc23fe0e7367c626f18fec4
Bhavana HK <bhavana.hk.1996@gmail.com>
2021-09-26 16:00:39 +0530

    removed dist

--- se-test/news-server/dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_KEY = exports.COOKIE_NAME = void 0;
-exports.COOKIE_NAME = 'qid';
-exports.API_KEY = '01a6ab0d75484f9094164cba8459db8d';
-//# sourceMappingURL=constants.js.map
